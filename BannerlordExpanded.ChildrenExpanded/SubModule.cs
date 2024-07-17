54d3ac38e1ccc73bcdeb3900c859719abd32dd3a
Axel Cawe <Cawe321Mods@gmail.com>
2024-07-17 19:26:07 +0800

    Age Settings Check whenever game starts

--- BannerlordExpanded.ChildrenExpanded/SubModule.cs
+++ BannerlordExpanded.ChildrenExpanded/SubModule.cs
@@ -1,9 +1,9 @@
-﻿using HarmonyLib;
-using System;
+﻿using BannerlordExpanded.ChildrenExpanded.Settings;
+using HarmonyLib;
 using System.Reflection;
-using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.ComponentInterfaces;
 using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
 
 
@@ -28,6 +28,34 @@ namespace BannerlordExpanded.ChildrenExpanded
             base.OnBeforeInitialModuleScreenSetAsRoot();
             new Harmony("BannerlordExpanded.ChildrenExpanded").PatchAll(Assembly.GetExecutingAssembly());
 
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            CheckSettingsValid();
+        }
+
+
+        void CheckSettingsValid()
+        {
+            if (Config.Instance.BecomeInfantAge > Config.Instance.BecomeChildAge)
+            {
+                InformationManager.DisplayMessage(new InformationMessage(new TextObject("{=BE_ChildrenExpanded_Settings_ChildrenLords_Error_01}[BE_ChildrenExpanded]: Mod Settings Error -> Become Infant Age value is greater than Become Child Age value. Mod has auto-patched for Become Child Age to align with Become Infant Age").ToString()));
+                Config.Instance.BecomeChildAge = Config.Instance.BecomeInfantAge;
+            }
+
+            if (Config.Instance.BecomeChildAge > Config.Instance.BecomeTeenagerAge)
+            {
+                InformationManager.DisplayMessage(new InformationMessage(new TextObject("{=BE_ChildrenExpanded_Settings_ChildrenLords_Error_02}[BE_ChildrenExpanded]: Mod Settings Error -> Become Child Age value is greater than Become Teenager Age value. Mod has auto-patched for Become Teenager Age to align with Become Child Age").ToString()));
+                Config.Instance.BecomeTeenagerAge = Config.Instance.BecomeChildAge;
+            }
+
+            if (Config.Instance.BecomeTeenagerAge > Config.Instance.HeroComesOfAge)
+            {
+                InformationManager.DisplayMessage(new InformationMessage(new TextObject("{=BE_ChildrenExpanded_Settings_ChildrenLords_Error_03}[BE_ChildrenExpanded]: Mod Settings Error -> Become Teenager Age value is greater than Hero Comes Of Age value. Mod has auto-patched for Hero Comes Of Age to align with Become Teenager Age").ToString()));
+                Config.Instance.HeroComesOfAge = Config.Instance.BecomeTeenagerAge;
+            }
         }
     }
 }
