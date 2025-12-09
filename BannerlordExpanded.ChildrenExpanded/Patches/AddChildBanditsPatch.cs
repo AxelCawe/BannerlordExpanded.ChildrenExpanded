55910677de9035599c81c3785eefbdf654854980
Cawe321 <cawe321Mods@gmail.com>
2025-12-09 21:29:07 +0800

    v1.3.9 Update

--- BannerlordExpanded.ChildrenExpanded/Patches/AddChildBanditsPatch.cs
+++ BannerlordExpanded.ChildrenExpanded/Patches/AddChildBanditsPatch.cs
@@ -1,25 +1,19 @@
 ﻿using BannerlordExpanded.ChildrenExpanded.Settings;
 using HarmonyLib;
-using MCM.Abstractions.Base.Global;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Roster;
-using TaleWorlds.Library;
 
 namespace Some_Bandits_Are_Now_Children.Patches
 {
-    [HarmonyPatch(typeof(MobileParty), "FillPartyStacks")]
+    [HarmonyPatch(typeof(MobileParty), "CreateParty")]
     public static class AddChildBanditsPatch
     {
         [HarmonyPostfix]
-        static void PostFix(MobileParty __instance, PartyTemplateObject pt, int troopNumberLimit = -1)
+        static void PostFix(ref MobileParty __result)
         {
-            if (__instance.IsBandit && Config.Instance.BanditChildrenActive)
+            if (__result.IsBandit && Config.Instance.BanditChildrenActive)
             {
                 // Correction
                 {
@@ -31,26 +25,25 @@ namespace Some_Bandits_Are_Now_Children.Patches
                     }
                 }
 
-                foreach (TroopRosterElement troop in __instance.MemberRoster.GetTroopRoster())
+                foreach (TroopRosterElement troop in __result.MemberRoster.GetTroopRoster())
                 {
-                    
+
                     CharacterObject childObject = CharacterObject.Find(troop.Character.StringId + "_children");
                     CharacterObject childFemaleObject = CharacterObject.Find(troop.Character.StringId + "_children_female");
 
                     float numberPercent = new Random().Next(Config.Instance.MinPercentOfBanditChildrenToAdd, Config.Instance.MaxPercentOfBanditChildrenToAdd + 1);
                     int numToAdd = TaleWorlds.Library.MathF.Floor(numberPercent / 100f * troop.Number);
                     if (childObject != null && childFemaleObject != null) numToAdd /= 2;
-
-                    if (childObject != null) 
+                    if (childObject != null)
                     {
-                        __instance.AddElementToMemberRoster(childObject, numToAdd);
-                        __instance.MemberRoster.RemoveTroop(troop.Character, numToAdd);
+                        __result.AddElementToMemberRoster(childObject, numToAdd);
+                        __result.MemberRoster.RemoveTroop(troop.Character, numToAdd);
                     }
-                        //mobileParty.AddElementToMemberRoster(childObject, MathF.Floor((float)GlobalSettings<MCMSettings>.Instance.minPercentOfChildrenToAdd / 100f * troop.Number)); 
+                    //mobileParty.AddElementToMemberRoster(childObject, MathF.Floor((float)GlobalSettings<MCMSettings>.Instance.minPercentOfChildrenToAdd / 100f * troop.Number)); 
                     if (childFemaleObject != null)
                     {
-                        __instance.AddElementToMemberRoster(childFemaleObject, numToAdd);
-                        __instance.MemberRoster.RemoveTroop(troop.Character, numToAdd);
+                        __result.AddElementToMemberRoster(childFemaleObject, numToAdd);
+                        __result.MemberRoster.RemoveTroop(troop.Character, numToAdd);
                     }
                 }
             }
--- BannerlordExpanded.ChildrenExpanded/Patches/VulnerableChildrenPatch.cs
+++ BannerlordExpanded.ChildrenExpanded/Patches/VulnerableChildrenPatch.cs
@@ -1,15 +1,10 @@
 ﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
 namespace BannerlordExpanded.ChildrenExpanded.Patches
 {
-    [HarmonyPatch(typeof(Agent), "EquipItemsFromSpawnEquipment", typeof(bool))]
+    [HarmonyPatch(typeof(Agent), "EquipItemsFromSpawnEquipment", typeof(bool), typeof(bool))]
     public static class VulnerableChildrenPatch
     {
         [HarmonyPostfix]
